e63bc84aa0c4af462f1a758abab425ed405e7f99
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-14 18:26:17 -0300

    feat: add user model with hashpassword

--- src/main/java/com/techforb/technicaltest/models/User.java
+++ src/main/java/com/techforb/technicaltest/models/User.java
@@ -1,5 +1,40 @@
 package com.techforb.technicaltest.models;
+import javax.persistence.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Entity
+@Table(name = "user")
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "number_document", nullable = false)
+    private String numberDocument;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "profile_image", nullable = false)
+    private String profileImage;
+
+    @Column(name = "type_document_id", nullable = false)
+    private Long typeDocumentId;
+
+    @ManyToOne
+    @JoinColumn(name = "type_document_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private TypeDocument typeDocument;
+
+     public void hashPassword() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        this.password = passwordEncoder.encode(this.password);
+    }
+
 }
