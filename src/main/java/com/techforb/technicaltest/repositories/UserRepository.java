b3a7ca110aac2ee8c44540b218a83d4a2766937c
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-15 14:40:43 -0300

    feat: search user by numberDocument

--- src/main/java/com/techforb/technicaltest/repositories/UserRepository.java
+++ src/main/java/com/techforb/technicaltest/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.techforb.technicaltest.repositories;
 import com.techforb.technicaltest.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByNumberDocument(String numberDocument);
+    Optional<User> findByNumberDocument(String numberDocument);
 
 }
