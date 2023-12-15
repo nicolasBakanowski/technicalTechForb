8602fa084ddd3de0e94e40d895f3e84d21400d29
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-14 23:00:15 -0300

    fix: turn off security for test

--- src/main/java/com/techforb/technicaltest/TechnicaltestApplication.java
+++ src/main/java/com/techforb/technicaltest/TechnicaltestApplication.java
@@ -2,8 +2,9 @@ package com.techforb.technicaltest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class TechnicaltestApplication {
 
 	public static void main(String[] args) {
