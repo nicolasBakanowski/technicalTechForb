3e13b52443bd5b4fa48000b48b711e3012228b8d
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-15 04:34:31 -0300

    add: card service

--- src/main/java/com/techforb/technicaltest/services/CardService.java
+++ src/main/java/com/techforb/technicaltest/services/CardService.java
@@ -1,5 +1,30 @@
 package com.techforb.technicaltest.services;
 
+import com.techforb.technicaltest.models.Card;
+import com.techforb.technicaltest.repositories.CardRepository;
+import com.techforb.technicaltest.utils.CardUtil;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CardService {
-    
+
+    private final CardRepository cardRepository;
+
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public void createCard(Card card) {
+        card.setCardNumber(CardUtil.generateRandomCardNumber());
+        card.setCcv(CardUtil.generateRandomCcv());
+        card.setExpirationDate(CardUtil.generateExpirationDate());
+
+        while (true) {
+            if (!cardRepository.existsByCardNumber(card.getCardNumber())) {
+                cardRepository.save(card);
+                return;
+            }
+            card.setCardNumber(CardUtil.generateRandomCardNumber());
+        }
+    }
 }
