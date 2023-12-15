018fe59bbe8d88e80ec7fbcf95752be52ac2772a
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-15 04:31:26 -0300

    feat: addCardRepository

--- src/main/java/com/techforb/technicaltest/repositories/CardRepository.java
+++ src/main/java/com/techforb/technicaltest/repositories/CardRepository.java
@@ -1,5 +1,10 @@
 package com.techforb.technicaltest.repositories;
 
-public class CardRepository {
-    
+import com.techforb.technicaltest.models.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+    boolean existsByCardNumber(String cardNumber);
 }
