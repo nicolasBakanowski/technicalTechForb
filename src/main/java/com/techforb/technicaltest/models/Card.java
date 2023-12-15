de9782fbddd466355d12c4a73afbc496d47d04b5
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-15 04:31:08 -0300

    feat:add card model

--- src/main/java/com/techforb/technicaltest/models/Card.java
+++ src/main/java/com/techforb/technicaltest/models/Card.java
@@ -1,5 +1,101 @@
 package com.techforb.technicaltest.models;
 
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.validation.constraints.*;
+import java.math.BigDecimal; 
+import java.util.Date;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "card")
+@Entity
 public class Card {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull(message = "El user_id no puede ser nulo")
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @NotNull
+    private String cardNumber;
+
+    @NotNull
+    private Integer ccv;
+
+    @NotNull
+    private Date expirationDate;
+
+    @Column(name = "amount", columnDefinition = "DECIMAL(10,2) DEFAULT 0.00", nullable = false)
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private User user;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Integer getCcv() {
+        return ccv;
+    }
+
+    public void setCcv(Integer ccv) {
+        this.ccv = ccv;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
