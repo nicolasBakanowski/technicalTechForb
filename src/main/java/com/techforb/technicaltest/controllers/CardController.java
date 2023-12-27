1d719d721192ae4d5ba9806657cc4325d94917a7
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-27 06:42:49 -0300

    generate new card from userID

--- src/main/java/com/techforb/technicaltest/controllers/CardController.java
+++ src/main/java/com/techforb/technicaltest/controllers/CardController.java
@@ -5,11 +5,12 @@ import com.techforb.technicaltest.services.CardService;
 import com.techforb.technicaltest.utils.AuthUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cards")
@@ -20,26 +21,50 @@ public class CardController {
     public CardController(CardService cardService) {
         this.cardService = cardService;
     }
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Card>> getAllUserCard(
+        @RequestHeader(name = "Authorization", required = false) String authorizationHeader
+    ) {
+        try {
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                String token = authorizationHeader.substring(7);
+                if (AuthUtil.validateToken(token)) {
+                    Long userId = AuthUtil.extractUserId(token);
+                    List<Card> userCards = cardService.getAllUserCardsByUserId(userId);
+                    return ResponseEntity.ok(userCards);
+                } else {
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null); 
+                }
+            }
+            else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+            }
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+         }
+        }
 
     @PostMapping("/create")
     public ResponseEntity<String> createCard(
-            @RequestBody Card card,
             @RequestHeader(name = "Authorization", required = false) String authorizationHeader
     ) {
         try {
             if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
                 String token = authorizationHeader.substring(7);
                 if (AuthUtil.validateToken(token)) {
-                    cardService.createCard(card);
+                    Long userId = AuthUtil.extractUserId(token);
+                    cardService.createCard(userId);
                     return new ResponseEntity<>("Card created successfully", HttpStatus.CREATED);
                 } else {
                     return new ResponseEntity<>("Invalid token", HttpStatus.UNAUTHORIZED);
                 }
-            } else {
+            }   
+            else {
                 return new ResponseEntity<>("Authorization header with Bearer token is required", HttpStatus.UNAUTHORIZED);
             }
         } catch (Exception e) {
             return new ResponseEntity<>("Error creating card: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    
 }
