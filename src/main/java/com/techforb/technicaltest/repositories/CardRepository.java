8a030b2148e02b6ff6506fc610d02988aaeec025
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-27 06:35:31 -0300

    add method findallbyuserID in card repository

--- src/main/java/com/techforb/technicaltest/repositories/CardRepository.java
+++ src/main/java/com/techforb/technicaltest/repositories/CardRepository.java
@@ -1,10 +1,12 @@
 package com.techforb.technicaltest.repositories;
-
 import com.techforb.technicaltest.models.Card;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
     boolean existsByCardNumber(String cardNumber);
+    List<Card> findAllByUserId(Long userId);
+
 }
